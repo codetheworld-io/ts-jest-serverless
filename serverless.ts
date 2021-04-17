150c68168af19fae5f7a552dc13338584c44a446
hoangdv <hoang.dv@outlook.com>
2021-04-17 17:08:40 +0900

    Add serverless-offline

--- serverless.ts
+++ serverless.ts
@@ -14,7 +14,10 @@ const serverlessConfiguration: AWS = {
       includeModules: true,
     },
   },
-  plugins: ['serverless-webpack'],
+  plugins: [
+    'serverless-webpack',
+    'serverless-offline',
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
